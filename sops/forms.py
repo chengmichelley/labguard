132dadedfd1edb365a9ec55ad2682b223bd3844d
Michelle Cheng <cheng.michelley@gmail.com>
2026-07-06 09:30:24 -0400

    Added Additional Form Input for SOPs Form

--- sops/forms.py
+++ sops/forms.py
@@ -4,10 +4,21 @@ from .models import SopDocument
 class SopDocumentForm(forms.ModelForm):
     class Meta:
         model = SopDocument
-        fields = ['title', 'doc_number', 'version', 'status']
+        fields = [
+            'title', 'doc_number', 'version', 'status', 'author_department', 'purpose_scope', 'roles_responsibilities', 'tools_equipment', 'procedure_steps', 'safety_compliance', 'approved_by', 'effective_date'
+        ]
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'doc_number': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g., SOP-LAB-001'}),
             'version': forms.NumberInput(attrs={'class': 'form-control'}),
             'status': forms.Select(attrs={'class': 'form-control'}),
+            'author_department': forms.TextInput(attrs={'class': 'form-control'}),
+            'approved_by': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'QA / Lab Manager Sign-off' }),
+            'effective_date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            
+            'purpose_scope': forms.Textarea(attrs={'class': 'form-control', 'rows':3}),
+            'roles_responsiblities': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+            'tools_equipment': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+            'procedure_steps': forms.Textarea(attrs={'class': 'form-control', 'rows': 5}),
+            'safety_compliance': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
         }
--- sops/models.py
+++ sops/models.py
@@ -14,5 +14,18 @@ class SopDocument(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
+    purpose_scope = models.TextField(blank=True, null=True, verbose_name='Purpose & Scope')
+    roles_responsibilities = models.TextField(blank=True, null=True, verbose_name='Roles & Responsibilities')
+    tools_equipment = models.TextField(blank=True, null=True, verbose_name='Required Tools & Equipment')
+    procedure_steps = models.TextField(blank=True, null=True, verbose_name='Procedure Steps')
+    safety_compliance = models.TextField(blank=True, null=True, verbose_name='Safety & Compliance')
+    
+    author_department = models.CharField(max_length=100, blank=True, null=True, verbose_name='Author/Department')
+    approved_by = models.CharField(max_length=100, blank=True, null=True, verbose_name='Approvals & Signatures')
+    effective_date = models.DateField(blank=True, null=True, verbose_name='Effective Date')
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    
     def __str__(self):
         return f'{self.doc_number}: {self.title} (v{self.version})'
